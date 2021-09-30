d25aebb2711aae2b86d75370f25d9e398e5890b8
Shubham Tambi <63859132+shubhamtambi27@users.noreply.github.com>
2021-09-30 19:58:21 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,3 +8,31 @@ def query(cmd):
 def date():
     now = os.system()
     return now
+
+
+#User Administration
+def create_user(username):
+    now=os.system("useradd "+username)
+    return now
+def currentuser():
+    now=os.system("whoami")
+    return now
+def createuser_nologin(username):
+    now=os.system("useradd -s /sbin/sh "+username)   
+    return now 
+def users():
+    now=os.system("cat /etc/shadow")
+    return now
+def user_info():
+    try:
+        now=os.system("cat /etc/passwd")
+        return now
+    except:
+        print("*****Access Denied !!*****")
+        print("*****You must be the root user to perform this action*****")
+def login_as_user(username):
+    try:
+        now=os.system("su "+username)
+        return now
+    except:
+        print("*****User does not exist or the user has no Shell*****")
