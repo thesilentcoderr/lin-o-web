540570674c1547c1afe3732a7b023e8a6cca8b5b
thesilentcoderr <gokulnama21@gmail.com>
2021-10-01 02:00:03 +0530

    Update in File System

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import os
+from subprocess import getstatusoutput as spo
 
 def query(cmd):
     if "date" in cmd:
@@ -36,3 +37,54 @@ def login_as_user(username):
         return now
     except:
         print("*****User does not exist or the user has no Shell*****")
+
+
+# File System
+def show_file(path,file_name):
+    try:
+        full_path = os.path.join(path,file_name)
+        output = spo(f'cat {full_path}')
+        if output and (output[0] == 0):
+            content = output[1]
+        else:
+            this_dir = spo('dir')[1]
+            content = f"No such file in the directory :- {this_dir}"
+    except:
+        content = "An error occured, Please try again !!"
+    return content
+    
+
+def create_file(path,file_name):
+    try:
+        full_path = os.path.join(path,file_name)
+        output = spo(f"touch {full_path}")
+        if output and (output[0] == 0):
+            msg = "File Created successfully"
+        else :
+            msg = f"Oops!!, {path} is NOT valid ."
+    except:
+        msg = "An error occured, Please try again !!"
+    return msg
+
+def create_dir(path,dir_name):
+    try:
+        full_path = os.path.join(path,dir_name)
+        output = spo(f"mkdir {full_path}")
+        if output and (output[0] == 0):
+            msg = "Folder/Directory Created successfully"
+        else :
+            msg = f"Oops!!, {path} is NOT valid ."
+    except:
+        msg = "An error occured, Please try again !!"
+    return msg
+
+def list_dir(path):
+    try:
+        output = spo(f"ls -l {path}")
+        if output and (output[0] == 0):
+            msg = output[1]
+        else :
+            msg = f"Oops!!, {path} is NOT valid ."
+    except:
+        msg = "An error occured, Please try again !!"
+    return msg
